eb16932d6798a9b0164add4b92052d5070c5d345
Andrii Myronenko <and.myron.dev@gmail.com>
2025-11-11 01:07:57 +0200

    added default values to application.properties, added testcontainers setup

--- src/test/java/com/example/banking/BankingApplicationTests.java
+++ src/test/java/com/example/banking/BankingApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.banking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class BankingApplicationTests {
 
     @Test
--- src/test/java/com/example/banking/TestcontainersConfiguration.java
+++ src/test/java/com/example/banking/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.banking;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -7,19 +8,26 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
+
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    @Value("${testcontainers.postgres.image}")
+    private String postgresImage;
+
+    @Value("${testcontainers.rabbitmq.image}")
+    private String rabbitmqImage;
 
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer<>(DockerImageName.parse(postgresImage));
     }
 
     @Bean
     @ServiceConnection
     RabbitMQContainer rabbitContainer() {
-        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
+        return new RabbitMQContainer(DockerImageName.parse(rabbitmqImage));
     }
 
 }
