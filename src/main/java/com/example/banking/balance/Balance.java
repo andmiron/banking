e1496b322c220f0f5e43f6845df51c043766b48c
Andrii Myronenko <and.myron.dev@gmail.com>
2025-11-11 17:34:09 +0200

    changed Balance model to POJO

--- src/main/java/com/example/banking/balance/Balance.java
+++ src/main/java/com/example/banking/balance/Balance.java
@@ -1,26 +1,21 @@
 package com.example.banking.balance;
 
 import com.example.banking.account.AccountStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Objects;
 
-public record Balance(
-        Long accountId,
-        AccountStatus accountStatus,
-        BalanceCurrency balanceCurrencyCode,
-        BigDecimal availableAmount,
-        Instant createdAt,
-        Instant updatedAt
-) {
-
-    public Balance {
-        Objects.requireNonNull(accountId, "Balance accountId must not be null");
-        Objects.requireNonNull(accountStatus, "Balance accountStatus must not be null");
-        Objects.requireNonNull(balanceCurrencyCode, "Balance balanceCurrencyCode must not be null");
-        Objects.requireNonNull(availableAmount, "Balance availableAmount must not be null");
-        Objects.requireNonNull(createdAt, "Balance createdAt must not be null");
-        Objects.requireNonNull(updatedAt, "Balance updatedAt must not be null");
-    }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Balance{
+        Long accountId;
+        AccountStatus accountStatus;
+        BalanceCurrencyCode balanceCurrencyCode;
+        BigDecimal availableAmount;
+        Instant createdAt;
+        Instant updatedAt;
 }
